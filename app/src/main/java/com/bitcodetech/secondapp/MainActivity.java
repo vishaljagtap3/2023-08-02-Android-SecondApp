b08cd069fcba39f908e16372ab4a9b4a9e126061
aavidsoft-developer <aavidsoft.developer@gmail.com>
2023-08-07 13:08:14 +0530

    Creating ui through xml

--- app/src/main/java/com/bitcodetech/secondapp/MainActivity.java
+++ app/src/main/java/com/bitcodetech/secondapp/MainActivity.java
@@ -3,12 +3,45 @@ package com.bitcodetech.secondapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imgLogo;
+    private EditText edtUsername, edtPassword;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgLogo = findViewById(R.id.imgLogo);
+        edtUsername = findViewById(R.id.edtUsername);
+        edtPassword = findViewById(R.id.edtPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new BtnLoginClickListener());
+
+    }
+
+    private class BtnLoginClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            if(edtUsername.getText().toString().equals("bitcode") && edtPassword.getText().toString().equals("bitcode")) {
+                Log.e("myapp", "Login Successful!");
+                Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT)
+                        .show();
+            }
+            else {
+                Log.e("myapp", "Login failed!");
+                Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
